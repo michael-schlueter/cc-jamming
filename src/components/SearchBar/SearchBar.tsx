d5c7e8c3b8c3208e669912092283dd0814717322
Michael Schlueter <schluet0r4life@gmail.com>
2023-05-08 20:38:44 +0200

    add functionality, types & JSX to SearchBar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,9 +1,28 @@
-import '.SearchBar.css';
+import { useState, useCallback } from "react";
+import ".SearchBar.css";
 
-function SearchBar() {
-    return (
-        <div>Hello SearchBar</div>
-    )
+interface SearchBarProps {
+  onSearch: (searchTerm: string) => void;
+}
+
+function SearchBar({ onSearch }: SearchBarProps) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const search = useCallback(() => {
+    onSearch(searchTerm);
+  }, [searchTerm, onSearch]);
+
+  return (
+    <div className="SearchBar">
+      <input
+        placeholder="Enter A Song Title"
+        onChange={(event) => setSearchTerm(event.target.value)}
+      />
+      <button className="SearchButton" onClick={search}>
+        SEARCH
+      </button>
+    </div>
+  );
 }
 
 export default SearchBar;
