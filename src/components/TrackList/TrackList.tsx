21171dc1bda25a64bb28d6cd34a875963effdc3b
Michael Schlueter <schluet0r4life@gmail.com>
2023-05-09 12:16:37 +0200

    add functionality, types & JSX to TrackList

--- src/components/TrackList/TrackList.tsx
+++ src/components/TrackList/TrackList.tsx
@@ -1,9 +1,36 @@
-import '.TrackList.css';
+import ".TrackList.css";
+import Track from "../Track/Track";
 
-function TrackList() {
-    return (
-        <div>Hello Playlist</div>
-    )
+interface Track {
+    id: string;
+    name: string;
+    artist: string;
+    album: string;
+    uri: string;
+  }
+interface TrackListProps {
+    tracks: Track[];
+    onAdd?: (track: Track) => void;
+    isRemoval?: boolean;
+    onRemove?: (track: Track) => void;
+}
+
+function TrackList({ tracks, isRemoval, onRemove, onAdd }: TrackListProps) {
+  return (
+    <div className="TrackList">
+      {tracks.map((track) => {
+        return (
+          <Track
+            track={track}
+            key={track.id}
+            onAdd={onAdd}
+            isRemoval={isRemoval}
+            onRemove={onRemove}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default TrackList;
